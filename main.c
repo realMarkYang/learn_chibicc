70acce96cdf641e0422f76ac7b1e3c37e0a0795d
realMarkYang <yangpyl@foxmail.com>
2026-09-05 22:27:51 +0800

    unary

--- main.c
+++ main.c
@@ -144,6 +144,7 @@ typedef enum
     ND_SUB, // -
     ND_MUL, // *
     ND_DIV, // /
+    ND_NEG, // unary - 一元运算符
     ND_NUM, // Integer
 } NodeKind;
 
@@ -172,6 +173,13 @@ static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
     return node;
 }
 
+static Node *new_unary(NodeKind kind, Node *expr)
+{
+    Node *node = new_node(kind);
+    node->lhs = expr;
+    return node;
+}
+
 static Node *new_num(int val)
 {
     Node *node = new_node(ND_NUM);
@@ -181,6 +189,7 @@ static Node *new_num(int val)
 
 static Node *expr(Token **rest, Token *tok);
 static Node *mul(Token **rest, Token *tok);
+static Node *unary(Token **rest, Token *tok);
 static Node *primary(Token **rest, Token *tok);
 
 // expr = mul ("+" mul | "-" mul)*
@@ -208,27 +217,40 @@ static Node *expr(Token **rest, Token *tok)
     }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 static Node *mul(Token **rest, Token *tok)
 {
-    Node *node = primary(&tok, tok);
+    Node *node = unary(&tok, tok);
     for (;;)
     {
         if (equal(tok, "*"))
         {
-            node = new_binary(ND_MUL, node, primary(&tok, tok->next));
+            node = new_binary(ND_MUL, node, unary(&tok, tok->next));
             continue;
         }
 
         if (equal(tok, "/"))
         {
-            node = new_binary(ND_DIV, node, primary(&tok, tok->next));
+            node = new_binary(ND_DIV, node, unary(&tok, tok->next));
             continue;
         }
         *rest = tok;
         return node;
     }
 }
+
+// unary = ("+" | "-") unary
+//       | primary
+static Node *unary(Token **rest, Token *tok) {
+  if (equal(tok, "+"))
+    return unary(rest, tok->next);
+
+  if (equal(tok, "-"))
+    return new_unary(ND_NEG, unary(rest, tok->next));
+
+  return primary(rest, tok);
+}
+
 
 // primary = "(" expr ")" | num
 static Node *primary(Token **rest, Token *tok)
@@ -268,9 +290,14 @@ static void pop(char *arg)
 // 后序遍历
 static void gen_expr(Node *node)
 {
-    if (node->kind == ND_NUM)
+    switch (node->kind)
     {
+    case ND_NUM:
         printf("  mov $%d, %%rax\n", node->val);
+        return;
+    case ND_NEG:
+        gen_expr(node->lhs);
+        printf("  neg %%rax\n");
         return;
     }
 
