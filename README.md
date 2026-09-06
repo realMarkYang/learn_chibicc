1869da43e1ac7d50fb51a30d9903b25366ac96c4
realMarkYang <yangpyl@foxmail.com>
2026-09-06 10:39:09 +0800

    support statements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learn_chibicc
 learn chibicc
+
+[http://scheme2006.cs.uchicago.edu/11-ghuloum.pdf]
---
+++ tokenize.c
@@ -0,0 +1,123 @@
+#include "chibicc.h"
+
+static char *current_input;
+
+bool equal(Token *tok, char *op)
+{
+    return memcmp(tok->loc, op, tok->len) == 0 && op[tok->len] == '\0';
+}
+
+// 使用可变参数列表来
+void error(char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    exit(1); // 错误退出
+}
+
+// 能指出错误在哪里的error
+void verror_at(char *loc, char *fmt, va_list ap)
+{
+    int pos = loc - current_input;
+    fprintf(stderr, "%s\n", current_input);
+    fprintf(stderr, "%*s", pos, ""); // print pos spaces.
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(loc, fmt, ap);
+}
+
+void error_tok(Token *tok, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    verror_at(tok->loc, fmt, ap);
+}
+
+// 确保跳过的就是s
+Token *skip(Token *tok, char *s)
+{
+    if (!equal(tok, s))
+        error_tok(tok, "except '%s'", s);
+    return tok->next;
+}
+
+static int get_number(Token *tok)
+{
+    if (tok->kind != TK_NUM)
+        error_tok(tok, "excepted a number");
+    return tok->val;
+}
+
+static bool startswith(char *p, char *q)
+{
+    return strncmp(p, q, strlen(q)) == 0;
+}
+
+// Read a punctuator token from p and returns its length.
+static int read_punct(char *p)
+{
+    if (startswith(p, "==") || startswith(p, "!=") ||
+        startswith(p, "<=") || startswith(p, ">="))
+        return 2;
+
+    return ispunct(*p) ? 1 : 0;
+}
+
+static Token *new_token(TokenKind kind, char *start, char *end)
+{
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->loc = start;
+    tok->len = end - start;
+    return tok;
+}
+
+// file start *p
+Token *tokenize(char *p)
+{
+    Token head = {};
+    current_input = p;
+    Token *cur = &head;
+
+    while (*p)
+    {
+        // skip whitespace characters
+        if (isspace(*p))
+        {
+            p++;
+            continue;
+        }
+
+        // parse number
+        if (isdigit(*p))
+        {
+            cur = cur->next = new_token(TK_NUM, p, p);
+            char *q = p;
+            cur->val = strtoul(p, &p, 10);
+            cur->len = p - q;
+            continue;
+        }
+
+        int punct_len = read_punct(p);
+        if (punct_len)
+        {
+            cur = cur->next = new_token(TK_PUNCT, p, p + punct_len);
+            p += cur->len;
+            continue;
+        }
+
+        error_at(p, "invalid token\n");
+    }
+
+    cur = cur->next = new_token(TK_EOF, p, p);
+    return head.next;
+}
